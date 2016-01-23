62ce8edc08c61ec3fb2dd9f3daa49c47d4638b3d
José Carlos Nieto <jose.carlos@menteslibres.net>
2016-01-22 21:04:06 -0500

    Drafting commands.

--- lib/hal/hal.go
+++ lib/hal/hal.go
@@ -2,6 +2,7 @@ package hal
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io"
 )
@@ -11,9 +12,19 @@ var space = []byte(` `)
 // Making sure this is a reader.
 var _ = io.Writer(&Hal{})
 
+var (
+	msgMissingCommand = `Say what?!`
+)
+
 type Hal struct {
 	out io.Writer
+	cwd string // current working directory.
 }
+
+var (
+	errMissingCommand    = errors.New(`Missing command.`)
+	errIncompleteCommand = errors.New(`Incomplete command.`)
+)
 
 func NewHal(out io.Writer) *Hal {
 	return &Hal{
@@ -25,16 +36,42 @@ func (h *Hal) Write(cmd []byte) (n int, err error) {
 	l := len(cmd)
 
 	chunks := bytes.Split(cmd, space)
-	if len(chunks) <= 1 {
-		s := string(chunks[0])
 
-		switch s {
-		case "help":
-			h.out.Write([]byte(`[repository]/[branch] [network] [target]`))
-			return l, nil
-		default:
-			h.out.Write([]byte(fmt.Sprintf("Unknown command %q", string(cmd))))
-			return l, nil
+	if len(chunks) < 1 {
+		h.out.Write([]byte(msgMissingCommand))
+		return l, errMissingCommand
+	}
+
+	s := string(chunks[0])
+
+	switch s {
+	case "help":
+		h.out.Write([]byte(`[repository]/[branch] [network] [target]`))
+		return l, nil
+	default:
+		if len(chunks) > 0 {
+			switch string(chunks[0]) {
+			case "set-repo":
+				if len(chunks) > 1 {
+					cwd := string(chunks[1])
+					if cwd != "" {
+						// TODO: check this is an actual repo.
+						h.cwd = cwd
+						h.out.Write([]byte(fmt.Sprintf("You current repo is %q", h.cwd)))
+						return l, nil
+					}
+				}
+				h.out.Write([]byte(fmt.Sprintf("Try `set-repo [repo-url]`")))
+				return l, errMissingCommand
+			}
+			if h.cwd != "" {
+				// TODO: insert sup magic here.
+				h.out.Write([]byte("Ok, done."))
+				return l, nil
+			} else {
+				h.out.Write([]byte(fmt.Sprintf("Missing repo, try `set-repo [repo-url]`")))
+			}
+			return l, errMissingCommand
 		}
 	}
 
--- lib/hal/hal_test.go
+++ lib/hal/hal_test.go
@@ -3,60 +3,50 @@ package hal
 import (
 	"bytes"
 	"github.com/stretchr/testify/assert"
+	"log"
 	"testing"
 )
 
+type testCommand struct {
+	command string
+	err     bool
+}
+
 func TestSendCommand(t *testing.T) {
 
-	testCommands := map[string]struct{ err bool }{
-		`help`: {
-			err: false,
-		}, // should show help.
-		/*
-			`deploy`: {
-				err: true,
-			}, // should fail because we didn't specify a network.
-			`prod deploy`: {
-				err: true,
-			}, // should fail because we don't have a repo.
-			`set-repo git@github.com:xiam/go-playground.git`: {
-				err: false,
-			}, // should set the repo
-			`prod deploy`: {
-				err: false,
-			}, // should execute production restart.
-			`git@github.com:xiam/go-playground.git prod deploy`: {
-				err: false,
-			}, // should set the repo and exec prod deploy
-			`git@github.com:xiam/go-playground.git prod deploy`: {
-				err: false,
-			}, // should set the repo and exec prod deploy
-			`git@github.com:xiam/vanity.git/issue-1 prod deploy`: {
-				err: false,
-			}, // should set the repo and exec prod deploy
-			`prod deploy`: {
-				err: false,
-			}, // should execute production deploy.
-			`git@github.com:xiam/go-playground.git prod deploy`: {
-				err: false,
-			}, // should set the repo and exec prod deploy
-			`prod deploy`: {
-				err: false,
-			}, // should execute production reploy.
-		*/
+	testCommands := []testCommand{
+		{
+			command: "help",
+			err:     false,
+		},
+		{
+			command: "prod deploy",
+			err:     true,
+		},
+		{
+			command: `set-repo git@github.com:xiam/go-playground.git`,
+			err:     false,
+		},
+		{
+			command: "prod deploy",
+			err:     false,
+		},
 	}
 
 	buf := bytes.NewBuffer(nil)
 
 	hal := NewHal(buf)
 
-	for c, v := range testCommands {
-		_, err := hal.Write([]byte(c))
+	for _, c := range testCommands {
+		_, err := hal.Write([]byte(c.command))
+		log.Printf("command: %q", c.command)
 		if err == nil {
-			assert.False(t, v.err)
+			assert.False(t, c.err)
 		} else {
-			assert.True(t, v.err)
+			assert.True(t, c.err)
 		}
+		log.Printf("out: %s, %q", buf.String(), err)
+		buf.Reset()
 	}
 
 }
